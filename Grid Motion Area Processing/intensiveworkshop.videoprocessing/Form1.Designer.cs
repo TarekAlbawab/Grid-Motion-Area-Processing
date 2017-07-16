637ca9a5cd9aad1ee91533f81213c37e4bb32cab
Tarek <tarek4u24@gmail.com>
2017-07-16 23:51:12 +0300

    Add files via upload

---
+++ Grid Motion Area Processing/intensiveworkshop.videoprocessing/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace intensiveworkshop.videoprocessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoSourcePlayerInput = new AForge.Controls.VideoSourcePlayer();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.listBoxHistory = new System.Windows.Forms.ListBox();
+            this.buttonHistory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayerInput
+            // 
+            this.videoSourcePlayerInput.Location = new System.Drawing.Point(12, 9);
+            this.videoSourcePlayerInput.Name = "videoSourcePlayerInput";
+            this.videoSourcePlayerInput.Size = new System.Drawing.Size(557, 486);
+            this.videoSourcePlayerInput.TabIndex = 0;
+            this.videoSourcePlayerInput.Text = "videoSourcePlayer1";
+            this.videoSourcePlayerInput.VideoSource = null;
+            this.videoSourcePlayerInput.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayerInput_NewFrame);
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(215, 501);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(171, 59);
+            this.buttonInsert.TabIndex = 1;
+            this.buttonInsert.Text = "INSERT VIDEO";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // listBoxHistory
+            // 
+            this.listBoxHistory.FormattingEnabled = true;
+            this.listBoxHistory.Location = new System.Drawing.Point(575, 9);
+            this.listBoxHistory.Name = "listBoxHistory";
+            this.listBoxHistory.Size = new System.Drawing.Size(377, 459);
+            this.listBoxHistory.TabIndex = 2;
+            // 
+            // buttonHistory
+            // 
+            this.buttonHistory.Location = new System.Drawing.Point(691, 501);
+            this.buttonHistory.Name = "buttonHistory";
+            this.buttonHistory.Size = new System.Drawing.Size(197, 59);
+            this.buttonHistory.TabIndex = 3;
+            this.buttonHistory.Text = "HISTORY";
+            this.buttonHistory.UseVisualStyleBackColor = true;
+            this.buttonHistory.Click += new System.EventHandler(this.buttonHistory_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 583);
+            this.Controls.Add(this.buttonHistory);
+            this.Controls.Add(this.listBoxHistory);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.videoSourcePlayerInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayerInput;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.ListBox listBoxHistory;
+        private System.Windows.Forms.Button buttonHistory;
+    }
+}
+
---
+++ Grid Motion Area Processing/intensiveworkshop.videoprocessing/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision.Motion;
+
+
+namespace intensiveworkshop.videoprocessing
+{
+    public partial class Form1 : Form
+    {
+        private readonly MotionDetector _detector;
+        private readonly List<string> _historyList; //global variable to be accessable to other loops
+        int count = 1;
+        private readonly GridMotionAreaProcessing _gridMotionAreaProcessing;
+        public Form1()
+        {
+            InitializeComponent();        
+            _detector = new MotionDetector(new SimpleBackgroundModelingDetector(), new MotionAreaHighlighting(Color.Purple));    
+            _historyList = new List<string>();
+        }
+
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog()==DialogResult.OK)
+            {
+                var videoSource = new FileVideoSource(openFileDialog.FileName);
+
+                OpenVideoSource(videoSource);
+            }
+            }
+
+        private void OpenVideoSource(IVideoSource source)
+        {
+            Cursor = Cursors.WaitCursor;
+
+            CloseCurrentVideoSource();//if you insert multiple times it will close the first one
+
+            videoSourcePlayerInput.VideoSource = source; 
+            videoSourcePlayerInput.Start();
+            Cursor = Cursors.Default;
+        }
+
+        private void CloseCurrentVideoSource()
+        {
+            if (videoSourcePlayerInput.VideoSource != null)
+            {
+                videoSourcePlayerInput.SignalToStop();
+                for (var i = 0; i < 30; i++)
+                {
+                    if (!videoSourcePlayerInput.IsRunning)
+                    {
+                        break;
+                    }
+                }
+                if (videoSourcePlayerInput.IsRunning)
+                {
+                    videoSourcePlayerInput.Stop();
+
+                }
+                videoSourcePlayerInput.VideoSource = null;
+            }
+        }
+
+        private void videoSourcePlayerInput_NewFrame(object sender, ref Bitmap image)//it enables the processing in the video that is in the video box
+  // it breaks the video into images 
+        {
+            var motionlevel = _detector.ProcessFrame(image);//motion level is threshold
+            var dateTime = DateTime.Now;
+            float i = image.Width;
+            float j = image.Height;
+            if (motionlevel > 0.05)
+            {
+                var historyString = "There was a motion at" + dateTime.ToString() + count.ToString();
+                _historyList.Add(historyString);
+            }
+           
+            var g = Graphics.FromImage(image);
+            var font = new Font(FontFamily.GenericSerif, 30);
+            var brush = new SolidBrush(Color.Red);
+            var pen = new SolidBrush(Color.Beige);                    
+            g.DrawString(dateTime.ToString(),  font, brush, new PointF(5, 5));
+            g.DrawString(count.ToString(),  font, brush, new PointF(40, 40));
+            brush.Dispose();
+            g.Dispose();
+            count ++;
+            
+
+        }
+
+        private void buttonHistory_Click(object sender, EventArgs e)//event loop
+        {
+            listBoxHistory.DataSource = _historyList;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
